59855803189c641640bcb3c5122ba4ca78386092
Aadarsh Nagrath <anagrath1@gmail.com>
2023-09-12 22:41:47 +0530

    Initial setup| - next, shadcn, clerk auth|

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h3 className="text-red-600">Hello World</h3>
+  )
+}
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+// Code here will be run before any route on the app is accessed - or any endpoint.
+// Its a safty code for application protection
+
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+    publicRoutes: ["/"],
+});
+ 
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+ 
